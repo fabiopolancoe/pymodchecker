4f37f13e817852a3ded5c9d39695bbc0615b28d2
Fabio <57909981+fabiopolancoe@users.noreply.github.com>
2020-05-28 17:11:07 -0500

    Upload main files

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo Welcome to PyChecker installer
+pip3 install tkinter guizero
+sudo apt-get install xterm
+cd /usr/local/bin
+sudo wget https://github.com/fabiopolancoe/pychecker/blob/master/pymodchecker.py
+sudo wget https://github.com/fabiopolancoe/pychecker/blob/master/pymodchecker.sh
+echo Installed, issue pymodchecker in terminal to run 
+
---
+++ pymodchecker.py
@@ -0,0 +1,24 @@
+import sys
+from subprocess import call
+from guizero import App, Text, PushButton, TextBox,info
+
+def search():
+    mod = str(packname.value)
+    bool = mod in sys.modules
+    if bool == True:
+        app.info("Success!", "The module have been found in the system")
+    elif bool == False:
+        install = app.yesno("Fail", "The module haven't been found in the system. Install module?")
+        if install == True:
+            app.info("Installing...", "Installing the module, please wait")
+            call(['xterm', '-e', 'pip3', 'install', mod])
+        else:
+            app.info("Okay!", "Okay, good luck Pythoneer!")
+
+app = App(title="PyChecker", height=200)
+label1 = Text(app, text="Welcome to PyChecker, let's check for installed Python modules")
+label2 = Text(app, text="Type the name of the module you're searching for")
+packname = TextBox(app, width=50)
+get = PushButton(app, text="Search module", command=search)
+
+app.display()
