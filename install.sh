c521a9a8a6c7b0283fc6788e3ee2df8dc3e18d50
Fabio <57909981+fabiopolancoe@users.noreply.github.com>
2020-05-28 21:27:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ echo Welcome to PyChecker installer
 pip3 install tkinter guizero
 cd /usr/bin
 sudo wget https://github.com/fabiopolancoe/pychecker/blob/master/pymodchecker.py
-sudo wget https://github.com/fabiopolancoe/pychecker/blob/master/pymodchecker
+sudo wget https://raw.githubusercontent.com/fabiopolancoe/pymodchecker/master/pymodchecker
 sudo chmod 777 pymodchecker
 echo Installed, issue pymodchecker in terminal to run 
