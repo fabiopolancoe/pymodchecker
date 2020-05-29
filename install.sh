593cfc37b1a67fa339fc61b3223e2102018576b8
Fabio <57909981+fabiopolancoe@users.noreply.github.com>
2020-05-29 13:01:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cd /usr/bin
 sudo wget https://raw.githubusercontent.com/fabiopolancoe/pymodchecker/master/pymodchecker.py
 sudo wget https://raw.githubusercontent.com/fabiopolancoe/pymodchecker/master/pymodchecker.sh
 sudo mv pymodchecker.sh pymodchecker
-sudo chmod 777 pymodchecker
+sudo chmod 777 pymodchecker pymodchecker.py
 echo Installed, issue pymodchecker in terminal to run 
 
